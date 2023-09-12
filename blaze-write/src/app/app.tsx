b4d59ed803668a42bba6db1528b99945cc23a444
Sanyam Arya <er.sanyam.arya@gmail.com>
2023-09-12 21:11:28 +0200

    Added FE project

---
+++ blaze-write/src/app/app.tsx
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled'
+
+const StyledApp = styled.div`
+  // Your style here
+`
+
+export function App() {
+  return (
+    <StyledApp>
+      <h1>
+        <span> Hello there, </span>
+        Welcome blaze-write 👋
+      </h1>
+    </StyledApp>
+  )
+}
+
+export default App
